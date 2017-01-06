12401d07339ae5a1fe997310bebe1d4d94cbebc9
Ricardo Perez <ararage@ricardos-macbook-pro.local>
2017-01-06 14:01:27 -0600

    Initial Commit

---
+++ True False TestTests/True_False_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  True_False_TestTests.swift
+//  True False TestTests
+//
+//  Created by Ricardo Perez on 1/6/17.
+//  Copyright © 2017 Ricardo Perez. All rights reserved.
+//
+
+import XCTest
+@testable import True_False_Test
+
+class True_False_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
